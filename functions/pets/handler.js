7f6bb6167728608d1666aca3f2b92f6b953b86e5
Partha <partha@myndbee.com>
2016-08-20 07:13:59 +0530

    AWS_IAM auth

--- functions/pets/handler.js
+++ functions/pets/handler.js
@@ -3,12 +3,27 @@ console.log('Loading function');
 
 let doc = require('dynamodb-doc');
 let dynamo = new doc.DynamoDB();
+let AWS = require('aws-sdk');
+let cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
 
 module.exports.handler = (event, context, callback) => {
-    let payload = {
-        "TableName": "pet",
-        "ProjectionExpression": "userid,id,#t,#n",
-        "ExpressionAttributeNames": {"#t": "type", "#n": "name"}
+    console.log('Lambda Event object', event);
+    console.log('Lambda context identity', JSON.stringify(context.identity));
+
+    var params = {
+      UserPoolId: 'us-east-1_48P4rvom0', /* required */
+      sub: '663ebc77-5357-4d42-8b4d-8852472ad077' /* required */
     };
-    dynamo.scan(payload,callback);
+    cognitoidentityserviceprovider.adminGetUser(params, function(err, data) {
+      if (err) console.log('Error ',err, err.stack); // an error occurred
+      else     console.log('User data',data);           // successful response
+      let payload = {
+          "TableName": "pet",
+          "ProjectionExpression": "userid,id,#t,#n",
+          "ExpressionAttributeNames": {"#t": "type", "#n": "name"}
+      };
+      dynamo.scan(payload,callback);
+    });
+
+
 };
